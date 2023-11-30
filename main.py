ee40811bf426c80265afec818dd68c4820d570a9
Clions1 <ahmetyunussarikan547@gmail.com>
2023-11-30 23:11:11 +0300

    BMI Calculator updated

--- main.py
+++ main.py
@@ -1,71 +1,46 @@
-import tkinter
-from tkinter import *
+import tkinter as tk
+from tkinter import messagebox
 
-window=Tk()
-window.title("BMI Calculator")
-window.minsize(400,400)
-#functions
 def Calculate():
-    weight =int(w_entry.get())
-    height =int(h_entry.get()) / 100
-    BMI = weight / (height*height)
-    if BMI < 18.5:
-        a_label.config(text=f"Your BMI is : {BMI} , You are Underweight")
-    elif BMI > 18.5 and BMI < 24.9:
-        a_label.config(text=f"Your BMI is : {BMI} , You are Normal weight")
-    elif BMI > 25 and BMI < 29.9:
-        a_label.config(text=f"Your BMI is : {BMI} , You are Over weight")
-    elif BMI > 30 and BMI < 34.9:
-        a_label.config(text=f"Your BMI is : {BMI} , You are Obesity (Class 1)")
-    elif BMI > 35 and BMI < 39.9:
-        a_label.config(text=f"Your BMI is : {BMI} , You are Obesity (Class 2)")
-    elif BMI > 40:
-        a_label.config(text=f"Your BMI is : {BMI} , You are Extreme Obesity")
+    try:
+        weight = int(w_entry.get())
+        height = int(h_entry.get()) / 100
+        BMI = weight / (height * height)
+        if BMI < 18.5:
+            a_label.config(text=f"Your BMI is : {BMI} , You are Underweight")
+        elif 18.5 <= BMI < 24.9:
+            a_label.config(text=f"Your BMI is : {BMI} , You are Normal weight")
+        elif 25 <= BMI < 29.9:
+            a_label.config(text=f"Your BMI is : {BMI} , You are Over weight")
+        elif 30 <= BMI < 34.9:
+            a_label.config(text=f"Your BMI is : {BMI} , You are Obesity (Class 1)")
+        elif 35 <= BMI < 39.9:
+            a_label.config(text=f"Your BMI is : {BMI} , You are Obesity (Class 2)")
+        elif BMI >= 40:
+            a_label.config(text=f"Your BMI is : {BMI} , You are Extreme Obesity")
+    except ValueError:
+        messagebox.showerror("Error", "Please Enter Correct Value")
 
+window = tk.Tk()
+window.title("BMI Calculator")
+window.minsize(400, 400)
 
-#functions
+w_label = tk.Label(text="Enter Your Weight (kg)")
+w_label.place(x=130, y=50)
 
-#weight_label
+w_entry = tk.Entry()
+w_entry.place(x=132, y=80)
 
-w_label=Label(text="Enter Your Weight (kg)")
-w_label.place(x=130,y=50)
+h_label = tk.Label(text="Enter Your Height (cm)")
+h_label.place(x=130, y=130)
 
-#weight_label
+h_entry = tk.Entry()
+h_entry.place(x=132, y=160)
 
-#height_entry
+button1 = tk.Button(text="Calculate", command=Calculate)
+button1.place(x=160, y=200)
 
-w_entry=Entry()
-w_entry.place(x=132,y=80)
-
-#height_entry
-
-#height_label
-
-h_label=Label(text="Enter Your Height (cm)")
-h_label.place(x=130,y=130)
-
-#height_label
-
-#height_entry
-
-h_entry=Entry()
-h_entry.place(x=132,y=160)
-
-#height_entry
-
-#button
-
-button1=Button(text="Calculate" , command=Calculate)
-button1.place(x=160,y=200)
-
-#button
-
-#answer_label
-
-a_label=Label()
-a_label.place(x=50,y=250)
-
-#answer_label
-
+a_label = tk.Label()
+a_label.place(x=50, y=250)
 
 window.mainloop()
